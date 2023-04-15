d3e2ef8bc8a660816fc8cb44a807f9cb34639182
Anuj Garg <anuj@Anujs-MacBook-Pro.local>
2023-04-15 10:52:02 +0530

    l3

---
+++ lecture_03/fsdemo.js
@@ -0,0 +1,6 @@
+let fs = require("fs");
+// let path = require("path");
+
+fs.mkdirSync("./somename");
+
+console.log(__dirname);
--- lecture_03/networkCall/index.js
+++ lecture_03/networkCall/index.js
@@ -1,18 +1,28 @@
 let axios = require('axios');
+let fs = require('fs');
+let path = require("path");
 
 let username = "keenwarrior";
-
-
-
 
 async function fetchUser(username){
     let url = "https://api.github.com/users/" + username;
     let resposne = await axios.get(url);
-    console.log(resposne.data.avatar_url);
-    return resposne.data;
+    if(resposne.status == 200){
+        let image_url = resposne.data.avatar_url;
+        let image_data = await axios({url: image_url, method: "get", responseType: "stream"});
+        let location = path.resolve(__dirname, "images", username+".png");
+        console.log(location);
+        let writer = fs.createWriteStream(location);
+        image_data.data.pipe(writer);
+    } else if(resposne.status == 404){
+        console.log("Wrong Username : "+username)
+    } else {
+        console.log("Something went wrong");
+    }
+    
 }
 
-let out = fetchUser("keenwarrior");
+let out = fetchUser("satheeshkumar3593");
 console.log("Hello");
 
 
